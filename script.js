d367cad9287712a537e2e9390f3e163f8b268dfe
Stanislav20 <stas_on38@mail.ru>
2023-02-06 20:55:46 +0800

    My first Mobile menu!

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+
+listItems.forEach((item, ind) => {
+	item.addEventListener('click', () => {
+		hideAllItems()	
+		hideAllContent()
+		item.classList.add('active')
+		contents[ind].classList.add('show')
+	})
+})
+
+function hideAllItems() {
+	listItems.forEach(item => item.classList.remove('active'))
+}
+
+function hideAllContent() {
+	contents.forEach(item => item.classList.remove('show'))
+}
